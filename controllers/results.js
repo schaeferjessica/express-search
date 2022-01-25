ab5e2f06bcbf0daef3345aebbca3ccd6daadd8fb
jschaefer <jessicaschaefer@hey.com>
2022-01-25 13:43:34 +0700

    feat(results): add dynamic data from controller result

--- controllers/results.js
+++ controllers/results.js
@@ -5,10 +5,53 @@ const router = express.Router()
 // views
 
 router.get('/', (req, res) => {
-  let users = ['miriam', 'nora']
-  res.render('results', {
-    users
-  });
+  let results = [
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      }, {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      },
+    ],
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      }, {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      },
+    ],
+  },
+  {
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [
+      {
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+      }, {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+      },
+    ],
+  }]
+  //console.log(req.body.search);
+  // funktion results = resultes ..
+  // contains the one title == req.body.search [{}]
+  res.render('results', {results})
 })
 
 // export module
